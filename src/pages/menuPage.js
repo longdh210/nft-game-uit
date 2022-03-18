966b68b402baac7d10c2332aa62654eb158ed248
Khoale38 <kevinle38@gmail.com>
2022-03-18 16:31:52 +0700

    [UPDATE] temp

--- src/pages/menuPage.js
+++ src/pages/menuPage.js
@@ -7,6 +7,11 @@ function Menu() {
     return (<div className="App">
         <div className="layout3">
             <img src={Icon} alt="Icon" className="logoMenuPage" />
+            <div className="missionBox">Mission:
+                Play with bot (0/10) -Reward 0.01 BTC
+                Play with bot (0/20) -
+                Reward 0.02 BTC
+            </div>
         </div>
         <div className="layout4">
             <img src={Group9} alt="corner" className="corner2" />
