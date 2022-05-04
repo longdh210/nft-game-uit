32db855f5344293d26fafe81b5e36d1e08da8537
Khoale38 <kevinle38@gmail.com>
2022-05-04 21:36:20 +0700

    [TEMP] new play screen

--- src/pages/playPage.js
+++ src/pages/playPage.js
@@ -1,71 +1,69 @@
-import "../styles/playPage.css";
-import CardBackSide from "../assets/CardBackside4.png";
-import UserAvt from "../assets/user-avt.png";
-import TempPcard from "../assets/TempPcard.png";
-import TempRcard from "../assets/TempRcard.png";
-import TempScard from "../assets/TempScard.png";
-import SupportIcon from "../assets/supportIcon.png";
+import '../styles/playPage.css'
+import CardBackSide from '../assets/CardBackside4.png'
+import UserAvt from '../assets/user-avt.png'
+import TempPcard from '../assets/TempPcard.png'
+import TempRcard from '../assets/TempRcard.png'
+import TempScard from '../assets/TempScard.png'
+import SupportIcon from '../assets/supportIcon.png'
 
-function Play() {
-    return (
-        <div className="playPage">
-            <div className="layoutFirst">
-                <div className="cornerPlayPage">
-                    <h2 className="cornerCount">Card left: 12</h2>
-                </div>
-                <div className="cardListPlayPage">
-                    <img className="card" src={CardBackSide} alt="Card back side" />
-                    <img className="card" src={CardBackSide} alt="Card back side" />
-                    <img className="card" src={CardBackSide} alt="Card back side" />
-                </div>
-                <div className="user-card">
-                    <div className="user-info">
-                        <img className="avt" src={UserAvt} alt="user-avt" />
-                        Player B
-                    </div>
-                    <div className="user-hp">HP: 5/5</div>
-                </div>
-            </div>
-            <div className="layoutSecond">
-            </div>
-            <div className="layoutThird">
-                <div className="user-card-2">
-                    <div className="user-info">
-                        <img className="avt" src={UserAvt} alt="user-avt" />
-                        Player B
-                    </div>
-                    <div className="user-hp">HP: 5/5</div>
-                </div>
-                <div className="cardListPlayPage-2">
-                    <img className="card" src={TempPcard} alt="Paper card" />
-                    <img className="card" src={TempRcard} alt="Rock card" />
-                    <img className="card" src={TempScard} alt="Scissor card" />
-                </div>
-                <div className="right-side">
-                    <div className="removed-card">
-                        <h1 style={{
-                            margin: "auto",
-                            fontSize: "20px",
-                            color: "white",
-                            textAlign: "center",
-                            justifyContent: "center",
-                            verticalAlign: "middle",
-                        }}>
-                            Removed Card
-                        </h1>
-                    </div>
-                    <div className="support-icon">
-                        <img 
-                            className="icon" 
-                            src={SupportIcon} 
-                            alt="support icon"
-                            style={{width: "45px"}}
-                        />
-                    </div>
-                </div>
-            </div>
+function Play () {
+  return (
+    <div className='playPage'>
+      <div className='layoutFirst'>
+        <div className='cornerCount'>
+          Card left: 12
         </div>
-    )
+        <div className='enemyCardList'>
+          <img className='card' src={CardBackSide} alt='Card back side' />
+          <img className='card' src={CardBackSide} alt='Card back side' />
+          <img className='card' src={CardBackSide} alt='Card back side' />
+        </div>
+        <div className='user-card'>
+          <div className='user-info'>
+            <img className='avt' src={UserAvt} alt='user-avt' /> Player B
+          </div>
+          <div className='user-hp'>
+            HP: 5/5
+          </div>
+        </div>
+      </div>
+      <div className='layoutSecond'>
+        COUNTDOWN:
+        <div>
+          15
+        </div>
+        PICK YOUR CARD
+      </div>
+      <div className='layoutThird'>
+        <div className='user-card-2' style={{'align-self': 'end'}}>
+          <div className='user-info'>
+            <img className='avt' src={UserAvt} alt='user-avt' /> Player B
+          </div>
+          <div className='user-hp'>
+            HP: 5/5
+          </div>
+        </div>
+        <div className='userCardList'>
+          <img className='card' src={TempPcard} alt='Paper card' />
+          <img className='card' src={TempRcard} alt='Rock card' />
+          <img className='card' src={TempScard} alt='Scissor card' />
+        </div>
+        <div className='removePlace'>
+          <div className='removeCard'>
+            <span>Removed Card</span>
+          </div>
+        </div>
+      </div>
+      <div className='supportIcon'>
+        <img
+          className='icon'
+          src={SupportIcon}
+          alt='support icon'
+          style={{width: '45px'}} />
+      </div>
+    </div>
+
+  )
 }
 
-export default Play;
+export default Play
